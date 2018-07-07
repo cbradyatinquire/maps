735cd49349af61ae5dda5ce89b14b56c333504b9
Corey Brady <cbrady@inquirelearning.com>
2018-07-06 23:56:46 -0500

    first auto generated extension

--- build.sbt
+++ build.sbt
@@ -1,8 +1,8 @@
 enablePlugins(org.nlogo.build.NetLogoExtension)
 
-netLogoExtName      := "graph"
+netLogoExtName      := "maps"
 
-netLogoClassManager := "GraphExtension"
+netLogoClassManager := "MapsExtension"
 
 scalaVersion           := "2.11.7"
 
--- src/main/java/GetObject.java
+++ src/main/java/GetObject.java
@@ -3,21 +3,18 @@ import org.nlogo.core.Syntax;
 import org.nlogo.core.SyntaxJ;
 
 public class GetObject implements Reporter {
-	
-  public Syntax getSyntax() {
-    return SyntaxJ.reporterSyntax(
-	new int[] {Syntax.StringType()}, Syntax.ListType());
-	//input name - returns creation string.
-  }
 
-  public Object report(Argument args[], Context context)
-      throws ExtensionException {
-    
+	public Syntax getSyntax() {
+		return SyntaxJ.reporterSyntax(
+			new int[] { Syntax.StringType() }, Syntax.ListType());
+	}
 
-	LogoListBuilder list = new LogoListBuilder();
-	list.add("example: ['my-triangle' 'Polygon(F, G, H)')'] ]");
-	return list.toLogoList();
-	
-  }
+	public Object report(Argument args[], Context context)
+		throws ExtensionException {
+			LogoListBuilder list = new LogoListBuilder();
+			list.add(42);
+			return list.toLogoList();
+
+	}
 
 }
